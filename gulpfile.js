30c2686b9e14201315e2c77d1a1f3fbd1d28d293
Joshua Hackett <joshua.hackett93@gmail.com>
2019-02-24 15:35:34 +0000

    proper gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,137 +2,134 @@ const { src, dest, watch, series, parallel } = require("gulp")
 const sass = require("gulp-sass")
 const webpack = require("webpack-stream")
 const path = require("path")
-const cleanCSS = require("gulp-clean-css")
-const browserSync = require('browser-sync').create();
+const cleanCss = require("gulp-clean-css")
+const imagemin = require('gulp-imagemin')
+const browserSync = require('browser-sync').create()
 
-sass.compiler = require("node-sass")
 
-// Run server
-const runServer = (cb) => {
+// Run live reload server
+const runServer = () => {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./dist"
         }
     })
-    watch("./*.html").on('change', browserSync.reload);
 }
 
 
+////////////
+// HTML
+////////////
+
+// Watch for HTML changes, then compile
+const watchHtml = () => {
+    watch(["./src/*.html"], (cb) => {
+        copyHtml()
+    })
+        .on('change', browserSync.reload)
+}
+// Copy HTML files
+const copyHtml = () => {
+    return src("./src/*.html")
+        .pipe(dest("dist"))
+}
+
+
+////////////
+// SASS
+////////////
+
+// Set sass compiler
+sass.compiler = require("node-sass")
+// Watch for sass changes, then compile
+const watchSass = () => {
+    watch(["src/sass/*"], (cb)=>{
+        compileSass()
+    })
+}
 // Compile sass
-const compileSass = (cb) => {
-    src("./src/main.sass")
+const compileSass = () => {
+    return src("./src/sass/main.sass")
         .pipe(sass({
             includePaths: ["node_modules"]
         }).on("error", sass.logError))
-        .pipe(dest("./"))
-    return cb()
+        .pipe(dest("dist/css"))
 }
-
-// Transpile js
-const transpileJs = (cb) => {
-    return src("./src/*.js")
-        .pipe(webpack( {
-            mode: "production",
-            entry: "./src/main.js",
-            module: {
-                rules: [
-                    {
-                        test: /\.css$/,
-                        use: ["css-loader"],
-                      },
-                    {
-                        test: /\.m?js$/,
-                        exclude: /(node_modules|bower_components)/,
-                        use: {
-                            loader: "babel-loader",
-                            options: {
-                                presets: [
-                                    ["env", 
-                                      {
-                                        targets: {
-                                          browsers: ["last 2 Chrome versions"]
-                                        }
-                                      }
-                                    ]
-                                ]
-                            }
-                        }
-                    }
-                ]
-            },
-            output: {
-                path: path.resolve(__dirname, "public"),
-                filename: "bundle.js"
-            }
-        }))
-        .pipe(dest("./"))
-}
-
 // Minify CSS
-const minifyCss = (cb) => {
+const minifyCss = () => {
     return src("./dist/*.css")
-        .pipe(cleanCSS({compatibility: "ie8"}))
-        .pipe(dest("./"))
+        .pipe(cleanCss({compatibility: "ie8"}))
+        .pipe(dest("dist/css"))
 }
 
-// Watch sass
-const watchSass = (cb) => {
-    watch(["src/*.sass", "src/*.css"], (cb)=>{
-        src("./src/main.sass")
-            .pipe(sass({
-                includePaths: ["node_modules"]
-            }).on("error", sass.logError))
-            .pipe(dest("./"))
-            .pipe(browserSync.stream())
-        return cb()
-    })
-    
+
+////////////
+// IMAGES
+////////////
+
+// Shrink images for production build
+const compressImages = () => {
+    return src("./src/img/*")
+        .pipe(imagemin())
+        .pipe(dest("dist/img"))
 }
 
+
+////////////
+// JS
+////////////
+
+// Production webpack configuration object
+let webpackConfig = {
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["css-loader"],
+              },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["env", 
+                              {
+                                targets: {
+                                  browsers: ["last 2 Chrome versions"]
+                                }
+                              }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    output: {
+        filename: "bundle.js"
+    }
+}
+// Transpile js
+const transpileJs = () => {
+    return src("./src/js/main.js")
+        .pipe(webpack(webpackConfig))
+        .pipe(dest("dist/js"))
+}
 // Watch js
 const watchJs = (cb) => {
-    return src("./src/*.js")
-        .pipe(webpack( {
-            mode: "development",
-            watch: true,
-            entry: "./src/main.js",
-            module: {
-                rules: [
-                    {
-                        test: /\.css$/,
-                        use: ["css-loader"],
-                      },
-                    {
-                        test: /\.m?js$/,
-                        exclude: /(node_modules|bower_components)/,
-                        use: {
-                            loader: "babel-loader",
-                            options: {
-                                presets: [
-                                    ["env", 
-                                      {
-                                        targets: {
-                                          browsers: ["last 2 Chrome versions"]
-                                        }
-                                      }
-                                    ]
-                                ]
-                            }
-                        }
-                    }
-                ]
-            },
-            output: {
-                path: path.resolve(__dirname, "public"),
-                filename: "bundle.js"
-            }
-        }))
-        .pipe(dest("./"))
-        .on('change', browserSync.reload)
+    webpackConfig.mode = "development"
+    webpackConfig.watch = true
+    src("./src/js/main.js")
+        .pipe(webpack(webpackConfig))
+        .pipe(dest("dist/js"))
+        .pipe(browserSync.stream())
 }
 
-// Production build task
-exports.build = series(compileSass, transpileJs, minifyCss)
 
+// Production build task
+exports.build = series(copyHtml, compileSass, minifyCss, transpileJs, compressImages)
 // Default watch task
-exports.default = parallel(watchSass, watchJs, runServer)
+exports.default = parallel(runServer, watchHtml, watchSass, watchJs)
